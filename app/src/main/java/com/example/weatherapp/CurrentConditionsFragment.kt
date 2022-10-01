e5013cc5863c14a3e099f4c29d0cb532714c03b9
rocky1661 <rockadock908@gmail.com>
2022-09-30 19:14:36 -0500

    assignment3

---
+++ app/src/main/java/com/example/weatherapp/CurrentConditionsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.weatherapp.databinding.FragmentCurrentConditionsBinding
+
+class CurrentConditionsFragment : Fragment(R.layout.fragment_current_conditions) {
+
+    private lateinit var binding: FragmentCurrentConditionsBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCurrentConditionsBinding.bind(view)
+        binding.forecastButton.setOnClickListener {
+            val forecast = Forecast(temp = "101")
+            val action = CurrentConditionsFragmentDirections.actionCurrentConditionsFragmentToForecastFragment(forecast)
+            findNavController().navigate(action)
+        }
+
+
+
+    }
+}
